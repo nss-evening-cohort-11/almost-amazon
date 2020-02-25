3d296793d41f96f96af7f2c06ffa9fa5ff89a0a4
Zoe Ames <zoeames@gmail.com>
2020-02-24 21:37:55 -0600

    cart prints

--- javascripts/components/store.js
+++ javascripts/components/store.js
@@ -3,7 +3,8 @@ import bookData from '../helpers/data/bookData.js';
 import cart from './cart.js';
 
 const addToCartEvent = () => {
-  cart.cartToDom();
+  const book = bookData.getBook();
+  cart.addToCart(book);
 };
 
 const makeStore = () => {
